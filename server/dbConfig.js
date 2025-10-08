3719745d6682111a4b53712141ed28f99b4e4c4c
Evan Clevenger <evanclevenger@gmail.com>
2025-10-08 06:16:31 -0500

    added mongoDB for user login

---
+++ server/dbConfig.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://evanclevenger_db_user:saVH7kJRjvHY3Lla@cluster0.x1xif3j.mongodb.net/"
+);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(`There was an error connecting to the DB: ${err}`);
+});
+
+module.exports = mongoose;
--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const User = require("./userModle"); //import from local module aka mongoose schema
 
 const app = express();
 app.use(cors());
+
+require("./dbConfig"); //connects the dbConfig.js file
 
 dotenv.config({ path: "../.env" }); // loads .env but tells it exactly where to look in the root
 const PORT = process.env.PORT || 5000;
